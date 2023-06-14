bfbc3377734e38ce6e550697a724696d69e023ec
4waggo <122998361+4waggo@users.noreply.github.com>
2023-06-14 17:44:22 +0300

    Update purchase_api.dart

--- lib/purchase_api.dart
+++ lib/purchase_api.dart
@@ -2,7 +2,7 @@ import 'package:flutter/services.dart';
 import 'package:purchases_flutter/purchases_flutter.dart';
 
 class PurchaseApi {
-  static const _apiKey = 'goog_OliVXcLaWqyuNZlebAQYUReaRTg';
+  static const _apiKey = '';
 
   static Future init() async {
     await Purchases.setDebugLogsEnabled(true);
